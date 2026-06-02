c207922f7653a7767ca219185e40e448b4f5aa7c
vcelkamarvin <laurinalbert@gmail.com>
2026-06-03 01:00:00 +0200

    Enable standalone Next.js output for Vercel deploy

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'standalone',
   experimental: {
     optimizePackageImports: ['@mui/material', '@mui/icons-material'],
   },
